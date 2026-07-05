ad6cbc9a3a8d15641f2ca8570197d503420e56f5
Harshal Mulay <harshalmulay1039@gmail.com>
2026-07-05 11:16:47 +0530

    origin allow in backend

--- Acadex Backend/src/main/java/com/acadex/config/SecurityConfig.java
+++ Acadex Backend/src/main/java/com/acadex/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://acadex-harshal.web.app", "https://acadex-harshal.firebaseapp.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
